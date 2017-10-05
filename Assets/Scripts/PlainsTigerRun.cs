7f642ca3c3f5b80ead2d3eb9a5ae5db511a0e7a4
takoda1 <31773273+takoda1@users.noreply.github.com>
2017-10-05 16:08:38 -0400

    Fixed tiger script and locations of water chests

---
+++ Assets/Scripts/PlainsTigerRun.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlainsTigerRun : MonoBehaviour {
+
+    public float MAX_RUN_BOUNDS = 100.0f;
+    private float current_progress;
+    private float direction;
+
+    // Use this for initialization
+    void Start()
+    {
+        current_progress = 0.0f;
+        direction = -1.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 updatedPosition = this.transform.position;
+        this.transform.position = new Vector3(updatedPosition.x + (direction * .1f), updatedPosition.y, updatedPosition.z);
+        current_progress += Mathf.Abs(direction * .1f);
+        if (current_progress > MAX_RUN_BOUNDS)
+        {
+            current_progress = 0;
+            Vector3 currentRotation = this.transform.rotation.eulerAngles;
+            if (direction == -1)
+            {
+                this.transform.rotation = Quaternion.Euler(currentRotation.x, currentRotation.y -180, currentRotation.z);
+            }
+            else
+            {
+                this.transform.rotation = Quaternion.Euler(currentRotation.x, currentRotation.y - 180, currentRotation.z);
+            }
+            direction *= -1;
+        }
+    }
+}
