ac43067646bcf8fc4d3c9e959fe7bad38b530823
Takoda <takoda.ren@gmail.com>
2017-11-20 13:44:38 -0500

    Added mainmenu scene, added return to main menu button functionality, started creating tutorial scene

---
+++ Assets/Scripts/Standard Scripts/ReturnToMainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnToMainMenu : MonoBehaviour {
+    private GameObject progression;
+
+    private void Start()
+    {
+        progression = GameObject.Find("_ProgressionManager");
+    }
+    // Update is called once per frame
+    void Update () {
+        OVRInput.Update();
+        if(OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger) && OVRInput.Get(OVRInput.Button.PrimaryTouchpad))
+        {
+            SceneManager.LoadScene("MainMenuScene");
+            GameObject.Destroy(progression);
+        }
+	}
+}
