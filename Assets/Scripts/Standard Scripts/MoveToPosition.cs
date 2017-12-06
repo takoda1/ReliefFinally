1fbecaa91e9bcff232f4d6769d23262a86cad607
Takoda <takoda.ren@gmail.com>
2017-12-06 15:49:37 -0500

    Started barnacle waters scene, added sound clips to worlds, started adding river/water effects to grassy plains, created a scene that goes through a set path in Grassy plains for people who are unable to use the controller to move around and linked to main menu

---
+++ Assets/Scripts/Standard Scripts/MoveToPosition.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToPosition : MonoBehaviour {
+
+    public Transform[] targetPositions;
+    public float speed;
+    private int counter = 0;
+	
+
+    void Start()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (Math.Pow(Math.Pow(targetPositions[counter].position.x - this.transform.position.x, 2)
+                   + Math.Pow(targetPositions[counter].position.z - this.transform.position.z, 2), .5) > 5)
+        {
+            Vector3 pos = Vector3.MoveTowards(transform.position, targetPositions[counter].position, speed * Time.deltaTime);
+            GetComponent<Rigidbody>().MovePosition(pos);
+        }
+        else
+        {
+            counter = (counter + 1) % targetPositions.Length;
+        }
+    }
+
+    IEnumerator wait(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+    }
+
+
+}
